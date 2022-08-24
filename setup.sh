332168be3296c34943a28e9b15248463d7c7869c
Humba01 <humbandroid@gmail.com>
2022-08-24 07:10:32 +0000

    Arrumando dotfiles.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 exec cp /.ssh/ /home/codespace/
 exec ls -al ~/.ssh 
-
-# id_rsa.pub
-# id_ecdsa.pub
-# id_ed25519.pub
+if [ -e id_rsa.pub || id_ecdsa.pub || id_ed25519.pub ]
+then
+    echo "[WARN\1] file exist."
+else
+    echo "[WARN\0] error configuration."
+    exit
+fi
