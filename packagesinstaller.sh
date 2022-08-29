b1326231732912e9b9aeed54bf43e4eb3867c13e
Humba01 <humbandroid@gmail.com>
2022-08-29 18:53:59 +0000

    Arrumando arquivos de dotfiles.

---
+++ packagesinstaller.sh
@@ -0,0 +1,3 @@
+echo "HN Dotfiles Global Package Installer [HNDGPI] v0.0.1"
+
+echo "Finished Instalation!"
--- setup.sh
+++ setup.sh
@@ -1,9 +1,3 @@
-exec cp /.ssh/ /home/codespace/
-exec ls -al ~/.ssh 
-if [ -e id_rsa.pub || id_ecdsa.pub || id_ed25519.pub ]
-then
-    echo "[WARN\1] file exist."
-else
-    echo "[WARN\0] error configuration."
-    exit
-fi
+echo "HN Dotfiles Global Configuration [HNDGC] v0.1"
+
+echo "Configuration Successfully!"
