f3711eb8b6982b4e9d27ca8541fba880b39dada8
Humba01 <humbandroid@gmail.com>
2022-08-29 19:08:24 +0000

    Arrumando arquivos de dotfiles.

--- packagesinstaller.sh
+++ packagesinstaller.sh
@@ -1,3 +1,7 @@
 echo "HN Dotfiles Global Package Installer [HNDGPI] v0.0.1"
 
+exec sudo apt-get update
+
+exec sudo apt-get install postgresql --assume-yes
+
 echo "Finished Instalation!"
--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 echo "HN Dotfiles Global Configuration [HNDGC] v0.1"
 
+exec sh packageinstaller.sh
+
 echo "Configuration Successfully!"
