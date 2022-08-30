524f8b2f66509dbad8b07eea01e671c914e58ad0
Humba01 <humbandroid@gmail.com>
2022-08-30 05:22:15 +0000

    Searando arquivos de configuração.

--- fixing/setup.sh
+++ fixing/setup.sh
@@ -1,6 +1,10 @@
 echo "\n\n======================================================"
 echo "\033[96;1mHN Dotfiles Global Configuration [HNDGC] v0.1\033[0m"
 
+exec sh configuration.sh
+
+exec sh variables.sh
+
 exec sh packages.sh
 
 echo "Configuration Successfully!"
