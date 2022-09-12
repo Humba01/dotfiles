e09326392aa8d1493bb3c4f1a272b53f40e03c43
Humba01 <humbandroid@gmail.com>
2022-09-12 08:42:43 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-echo "HN Dotfiles Global Configuration [HNDGC]"
+echo "Dotfiles Global Configuration v1"
 exec sudo apt-get update
-exec sudo apt-get install python2.7 sqlite3 sqlite3-doc postgresql --assume-yes
+exec sudo apt-get install -y python2.7 sqlite3 sqlite3-doc postgresql npm nodejs
