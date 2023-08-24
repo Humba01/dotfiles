dc1a27abc1a2096551c1b19e194d86ebf7f5051d
Humberto Bravo <humbandroid@gmail.com>
2023-08-24 00:02:07 +0000

    arrumando repo

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
-echo "Dotfiles Global Configuration v1"
+echo "Dotfiles Global Codespaces Packages"
+
 exec sudo apt-get update
+
 exec sudo apt-get install -y python2.7 sqlite3 sqlite3-doc postgresql npm nodejs
+
+exec code --install-extension ./flawul-0.0.4.vsix
+
